461d1648b6b3114552a898018e5b9351bc0e9504
hyejinlim <yoon1440_@naver.com>
2022-02-20 15:16:46 +0900

    feat: list + grid

--- Week2/listview_example/lib/main.dart
+++ Week2/listview_example/lib/main.dart
@@ -56,7 +56,6 @@ List<Map> listData = [
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -64,42 +63,96 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Listview Example'),
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20.0,
-                vertical: 10.0,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('전체 ${listData.length}개'),
-                  Icon(Icons.list),
-                ],
-              ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isGrid = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Listview Example'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20.0,
+              vertical: 10.0,
             ),
-            Expanded(
-              child: ListView.builder(
-                // primary: false,
-                itemCount: listData.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return BuildListTile(
-                    title: listData[index]['title'],
-                    subTitle: listData[index]['subTitle'],
-                    icon: listData[index]['icon'],
-                  );
-                },
-              ),
-            )
-          ],
-        ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('전체 ${listData.length}개'),
+                IconButton(
+                  onPressed: handleListType,
+                  icon: (isGrid ? Icon(Icons.list) : Icon(Icons.grid_view)),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: (isGrid
+                  ? GridView.builder(
+                      itemCount: listData.length,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2),
+                      itemBuilder: (BuildContext context, int index) {
+                        return Column(
+                          children: [
+                            Icon(
+                              listData[index]['icon'],
+                              size: 40,
+                            ),
+                            Container(
+                              child: Text(
+                                listData[index]['title'],
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            Container(
+                              child: Text(listData[index]['subTitle']),
+                            ),
+                          ],
+                        );
+                      },
+                    )
+                  : ListView.builder(
+                      // primary: false,
+                      itemCount: listData.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return BuildListTile(
+                          title: listData[index]['title'],
+                          subTitle: listData[index]['subTitle'],
+                          icon: listData[index]['icon'],
+                        );
+                      },
+                    )),
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  void handleListType() {
+    setState(() {
+      isGrid ? isGrid = false : isGrid = true;
+    });
   }
 }
 
