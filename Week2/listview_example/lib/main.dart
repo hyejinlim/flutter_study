fd1e4fd801618107e17866bedb2eeca8c0541834
hyejinlim <yoon1440_@naver.com>
2022-02-20 15:36:25 +0900

    feat: data model 분리

--- Week2/listview_example/lib/main.dart
+++ Week2/listview_example/lib/main.dart
@@ -1,57 +1,9 @@
 import 'package:flutter/material.dart';
+import '../models/location.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-List<Map> listData = [
-  {
-    'title': 'CineArts at the Empire',
-    'subTitle': '85 W Portal Ave',
-    'icon': Icons.theaters
-  },
-  {
-    'title': 'The Castro Theater',
-    'subTitle': '429 Castro St',
-    'icon': Icons.theaters
-  },
-  {
-    'title': 'Alamo Drafthouse Cinema',
-    'subTitle': '2550 Mission St',
-    'icon': Icons.theaters
-  },
-  {
-    'title': 'Roxie Theater',
-    'subTitle': '3117 16th St',
-    'icon': Icons.theaters
-  },
-  {
-    'title': 'United Artists Stonestown Twin',
-    'subTitle': '501 Buckingham Way',
-    'icon': Icons.theaters
-  },
-  {
-    'title': 'AMC Metreon 16',
-    'subTitle': '135 4th St #3000',
-    'icon': Icons.theaters
-  },
-  {
-    'title': 'K\'s Kitchen',
-    'subTitle': '757 Monterey Blvd',
-    'icon': Icons.restaurant
-  },
-  {
-    'title': 'Emmy\'s Restaurant',
-    'subTitle': '1923 Ocean Ave',
-    'icon': Icons.restaurant
-  },
-  {
-    'title': 'Chaiya Thai Restaurant',
-    'subTitle': '272 Claremont Blvd',
-    'icon': Icons.restaurant
-  },
-  {'title': 'La Ciccia', 'subTitle': '291 30th St', 'icon': Icons.restaurant},
-];
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -94,7 +46,7 @@ class _HomeState extends State<Home> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text('전체 ${listData.length}개'),
+                Text('전체 ${locations.length}개'),
                 IconButton(
                   onPressed: handleListType,
                   icon: (isGrid ? Icon(Icons.list) : Icon(Icons.grid_view)),
@@ -107,25 +59,26 @@ class _HomeState extends State<Home> {
               padding: const EdgeInsets.all(8.0),
               child: (isGrid
                   ? GridView.builder(
-                      itemCount: listData.length,
+                      itemCount: locations.length,
                       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                           crossAxisCount: 2),
                       itemBuilder: (BuildContext context, int index) {
                         return Column(
                           children: [
                             Icon(
-                              listData[index]['icon'],
+                              locations[index].icon,
                               size: 40,
+                              color: Colors.blue[500],
                             ),
                             Container(
                               child: Text(
-                                listData[index]['title'],
+                                locations[index].title,
                                 maxLines: 1,
                                 overflow: TextOverflow.ellipsis,
                               ),
                             ),
                             Container(
-                              child: Text(listData[index]['subTitle']),
+                              child: Text(locations[index].subTitle),
                             ),
                           ],
                         );
@@ -133,12 +86,12 @@ class _HomeState extends State<Home> {
                     )
                   : ListView.builder(
                       // primary: false,
-                      itemCount: listData.length,
+                      itemCount: locations.length,
                       itemBuilder: (BuildContext context, int index) {
                         return BuildListTile(
-                          title: listData[index]['title'],
-                          subTitle: listData[index]['subTitle'],
-                          icon: listData[index]['icon'],
+                          title: locations[index].title,
+                          subTitle: locations[index].subTitle,
+                          icon: locations[index].icon,
                         );
                       },
                     )),
